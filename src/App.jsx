765769857bb26da2f0ace57bfb6bada40edac88b
CaiXinRu <claire_129as@yahoo.com.tw>
2023-12-01 10:57:54 +0800

    feat: 新增Router + Home
    feat: 新增Router + Home-2

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+import { AuthProvider } from "./context/AuthContext";
+import Header from "./component/Header";
+import Home from "./pages/Home";
+import LogIn from "./pages/LogIn";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Container>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<LogIn />} />
+          </Routes>
+        </Container>
+      </AuthProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
